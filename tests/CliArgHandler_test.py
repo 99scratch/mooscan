de9b0337034262e44915a3eee0263434d2760a58
James McLean <james.mclean@gmail.com>
2017-11-01 22:13:13 +1030

    Added the start of some tests

---
+++ tests/CliArgHandler_test.py
@@ -0,0 +1,48 @@
+import argparse
+import pytest
+from lib.core.CliArgHandler import CliArgHandler
+
+valid_test_site = 'http://www.testsite.com'
+
+def test_cliarg_default_values():
+
+    # Arrange
+    argv = []
+
+    # Act
+    parser = CliArgHandler()
+    arguments = parser.parse(argv)
+
+    expected_args = {
+            'allscans': True, 
+            'htaccess': False, 
+            'update': False, 
+            'url': None, 
+            'verbose': None, 
+            'version': False
+    }
+
+    # Assert
+    assert vars(arguments) == expected_args
+
+def test_cliarg_host_value():
+
+    # Arrange
+    argv = ['--url', valid_test_site]
+
+    # Act
+    parser = CliArgHandler()
+    arguments = parser.parse(argv)
+
+    expected_args = {
+        'allscans': True,
+        'htaccess': False,
+        'update': False,
+        'url': valid_test_site,
+        'verbose': None,
+        'version': False
+    } 
+
+    # Assert
+    assert vars(arguments) == expected_args
+
