94c461fbb88260c786c85b863e8fe71414c33bbe
James McLean <james.mclean@gmail.com>
2017-11-04 12:33:52 +1030

    Added tests for url validator with responses mock library

--- tests/CliArgHandler_test.py
+++ tests/CliArgHandler_test.py
@@ -1,5 +1,7 @@
 import argparse
 import pytest
+import responses
+from pytest_mock import mocker
 from lib.core.CliArgHandler import CliArgHandler
 
 
@@ -49,7 +51,7 @@ def test_cliarg_host_value():
     # Assert
     assert vars(arguments) == expected_args
 
-
+@responses.activate
 def test_invalid_url_format():
     
     # Arrange
@@ -61,3 +63,47 @@ def test_invalid_url_format():
     # Assert
     with pytest.raises(SystemExit):
         parser.parse(argv)
+
+@responses.activate
+def test_url_not_moodle_validate():
+
+    # Arrange
+    httpurl = 'http://somesite.com'
+    responses.add(responses.GET, httpurl + '/login/index.php', status=404)
+    responses.add(responses.GET, httpurl + '/lib/db/install.xml', status=404)
+
+    argv = ['--url', httpurl]
+
+    # Act
+    parser = CliArgHandler()
+
+    # Assert
+    with pytest.raises(SystemExit):
+        parser.parse(argv)
+
+@responses.activate
+def test_url_is_moodle_validate():
+
+    # Arrange
+    httpurl =  'http://somesite.com'
+    responses.add(responses.GET, httpurl + '/login/index.php', status=200)
+    responses.add(responses.GET, httpurl + '/lib/db/install.xml', status=200)
+
+    argv = ['--url', httpurl]
+
+    expected_arguments = {
+        'allscans': True,
+        'htaccess': False,
+        'update': False,
+        'url': httpurl,
+        'verbose': None,
+        'version': False
+    }
+
+    # Act 
+    parser = CliArgHandler()
+    arguments = parser.parse(argv)
+
+    # Assert
+    assert vars(arguments) == expected_arguments
+
