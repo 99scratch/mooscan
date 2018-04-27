a9d81b1db8663f891e263340752e96a4be5b739a
James McLean <james.mclean@uxcsaltbush.com.au>
2018-04-27 11:48:16 +0930

    Added test for check config (to be implemented)

--- tests/test_ConfigHandler.py
+++ tests/test_ConfigHandler.py
@@ -12,3 +12,24 @@ def test_env_path():
 
     # Assert
     assert handler.GetConfigFile() == '/some/new/path/mooscan.conf'
+
+def test_config_validate():
+
+    # Arrange
+    config = {
+            'mooscan_path': '~/.mooscan', 
+            'git_path': 'moodle-git', 
+            'update_module_freq': 14, 
+            'update_code_freq': 14, 
+            'moodle_git': 'https://github.com/moodle/moodle.git', 
+            'user_agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0', 
+            'module_database': 'modules.db', 
+            'versions_database': 'versions.db'
+    }
+
+    # Act
+    handler = ConfigHandler()
+    handler.LoadConfig()
+
+    # Assert
+    assert handler.GetLoadedConfig() == config
