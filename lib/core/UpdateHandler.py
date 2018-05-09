200f27f40bceff6c51fa92192a0e824f8557aae4
James McLean <james.mclean@uxcsaltbush.com.au>
2018-05-09 19:41:17 +0930

    Work on importing the Moodle code versions and filenames into the sqlite db

--- lib/core/UpdateHandler.py
+++ lib/core/UpdateHandler.py
@@ -3,6 +3,8 @@ import subprocess
 import time
 import json
 import requests
+import glob
+import re
 from git import Repo
 from lib.core.TextHandler import TextHandler
 from lib.core.DatabaseHandler import DatabaseHandler
@@ -33,13 +35,13 @@ class UpdateHandler(object):
         # Check if the git directory is present
         if os.environ.get('MOOSCAN_DATA_PATH'):
             TextHandler().debug("Environment variable MOOSCAN_DATA_PATH is "
-                                "set, ignoring config value")
+                    "set, ignoring config value")
             mooscan = os.environ.get('MOOSCAN_DATA_PATH')
         else:
             mooscan = self.config['mooscan_path']
 
         path = "{mooscan}/{git}".format(mooscan=mooscan,
-                                        git=self.config['git_path'])
+                git=self.config['git_path'])
 
         self.gitpath = os.path.expanduser(path)
 
@@ -47,22 +49,81 @@ class UpdateHandler(object):
 
         if(os.path.exists(self.gitpath)):
             TextHandler().debug("Moodle code discovered at {dir}. "
-                                "Getting latest."
-                                .format(dir=self.gitpath))
+                    "Getting latest."
+                    .format(dir=self.gitpath))
             repo = Repo(self.gitpath)
+            repo.git.checkout('master', force=True)
             pull = repo.remotes.origin
             pull.pull()
             TextHandler().debug("Done")
         else:
             TextHandler().debug("Creating target git repository at {dir}"
-                                .format(dir=self.gitpath))
+                    .format(dir=self.gitpath))
             os.makedirs(self.gitpath)
             TextHandler().debug("Pulling the Moodle Git repo from {url}"
-                                .format(url=self.config['moodle_git']))
+                    .format(url=self.config['moodle_git']))
             Repo.clone_from(self.config['moodle_git'], self.gitpath)
             TextHandler().debug("Done")
 
+        self.git_parse_repo()
+
         self.db.save_updates('code')
+
+    def git_parse_repo(self):
+        # Loop through all tagged versions of Moodle in the git repo
+        # This is honestly pretty bad. But until I decide to do this
+        # "in the cloud" and offer a service for clients to update from
+        # with this pre-parsed, it'll have to do...
+
+        moodlegit = Repo(self.gitpath)
+        tags = moodlegit.tags
+
+        for tag in tags:
+            if re.match('^v1.[0-6]', tag.name):
+                print(tag.name)
+                # unset the item from the list
+                tags.remove(tag)
+
+        TextHandler().info("Processing Moodle tagged versions...")
+        for tag in tags:
+            moodlegit.git.checkout(tag.name, force=True)
+
+            # Nothing of interest is in Moodle before 1.7..
+
+            print("Tag: {tag}".format(tag=tag.name), end='')
+
+            # If there is no lib/db/install.xml, we don't care. It's too old.
+            # You'll have to go old-school..
+            if not os.path.exists(self.gitpath + '/lib/db/install.xml'):
+                print("\r", end='')
+                continue
+
+            globpath = "{path}/**/*".format(path=self.gitpath)
+            for strmdlfile in glob.glob(globpath, recursive=True):
+                print(strmdlfile, flush=True)
+                # Hash the file content itself
+                strmdlfile = os.path.normpath(self.gitpath + "/" + str(mdlfile.decode()))
+
+                if os.path.isdir(strmdlfile):
+                    continue
+
+                ignore = ['.php','.gif','.png','.jpg','.git']
+                if strmdlfile in ignore:
+                    continue
+
+                filehash = hashlib.md5()
+                try:
+                    with open(strmdlfile, "rb") as f:
+                        for chunk in iter(lambda: f.read(BUF), b""):
+                            filehash.update(chunk)
+
+                    print(strmdlfile + " MD5: " + filehash.hexdigest())
+
+                    # If the file is 'install.xml' parse it, pull out the version number, save that too.
+                    if 'install.xml' in strmdlfile:
+                        print("**install.xml found** : {file}".format(file=strmdlfile))
+                except:
+                    print("FNF {file}".format(file=strmdlfile))  
 
     def git_update_required(self):
         updatedata = self.db.get_updates()
@@ -137,10 +198,10 @@ class UpdateHandler(object):
             query = self.update_query(batch)
 
             headers = {
-                'User-Agent': self.config['user_agent'],
-                'Content-Type': 'application/json',
-                'Content-Length': len(query)
-            }
+                    'User-Agent': self.config['user_agent'],
+                    'Content-Type': 'application/json',
+                    'Content-Length': len(query)
+                    }
 
             url = 'https://moodle.org/lib/ajax/service.php'
 
